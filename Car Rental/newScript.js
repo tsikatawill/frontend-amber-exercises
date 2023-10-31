10e45f4ac55399a8c6098e79e5e742591a97922f
William M. Tsikata <tsikatawill@gmail.com>
2023-10-31 21:28:21 +0000

    implement javascript forEach loop

---
+++ Car Rental/newScript.js
@@ -0,0 +1,341 @@
+const CARS = [
+  {
+    image: "./assets/images/1.jpg",
+    name: "VW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/2.png",
+    name: "BMW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/4.png",
+    name: "Benz",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/5.jpg",
+    name: "Toyota",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/6.jpg",
+    name: "Rolls Royce",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/7.jpg",
+    name: "Chrysler",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/1.jpg",
+    name: "VW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/2.png",
+    name: "BMW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/4.png",
+    name: "Benz",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/5.jpg",
+    name: "Toyota",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/6.jpg",
+    name: "Rolls Royce",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/7.jpg",
+    name: "Chrysler",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/1.jpg",
+    name: "VW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/2.png",
+    name: "BMW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/4.png",
+    name: "Benz",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/5.jpg",
+    name: "Toyota",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/6.jpg",
+    name: "Rolls Royce",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/7.jpg",
+    name: "Chrysler",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/1.jpg",
+    name: "VW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/2.png",
+    name: "BMW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/4.png",
+    name: "Benz",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/5.jpg",
+    name: "Toyota",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/6.jpg",
+    name: "Rolls Royce",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/7.jpg",
+    name: "Chrysler",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/1.jpg",
+    name: "VW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/2.png",
+    name: "BMW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/4.png",
+    name: "Benz",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/5.jpg",
+    name: "Toyota",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/6.jpg",
+    name: "Rolls Royce",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/7.jpg",
+    name: "Chrysler",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/1.jpg",
+    name: "VW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/2.png",
+    name: "BMW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/4.png",
+    name: "Benz",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/5.jpg",
+    name: "Toyota",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/6.jpg",
+    name: "Rolls Royce",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/7.jpg",
+    name: "Chrysler",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/1.jpg",
+    name: "VW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/2.png",
+    name: "BMW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/4.png",
+    name: "Benz",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/5.jpg",
+    name: "Toyota",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/6.jpg",
+    name: "Rolls Royce",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/7.jpg",
+    name: "Chrysler",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/1.jpg",
+    name: "VW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/2.png",
+    name: "BMW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/4.png",
+    name: "Benz",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/5.jpg",
+    name: "Toyota",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/6.jpg",
+    name: "Rolls Royce",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/7.jpg",
+    name: "Chrysler",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/1.jpg",
+    name: "VW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/2.png",
+    name: "BMW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/4.png",
+    name: "Benz",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/5.jpg",
+    name: "Toyota",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/6.jpg",
+    name: "Rolls Royce",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/7.jpg",
+    name: "Chrysler",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/1.jpg",
+    name: "VW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/2.png",
+    name: "BMW",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/4.png",
+    name: "Benz",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/5.jpg",
+    name: "Toyota",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/6.jpg",
+    name: "Rolls Royce",
+    rate: "1850gh/day",
+  },
+  {
+    image: "./assets/images/7.jpg",
+    name: "Chrysler",
+    rate: "1850gh/day",
+  },
+];
+
+const cards = document.querySelector(".cards");
+
+const CarNodes = [];
+
+CARS.forEach((car) => {
+  const carCard = document.createElement("div");
+  carCard.setAttribute("class", "card-col");
+
+  const carImg = document.createElement("img");
+  carImg.setAttribute("class", "card-img");
+
+  const carImgText = document.createElement("div");
+  carImgText.setAttribute("class", "img-text");
+
+  const carName = document.createElement("h4");
+  const carRate = document.createElement("span");
+
+  const bookNowBtn = document.createElement("button");
+  bookNowBtn.setAttribute("id", "rent");
+
+  carImg.setAttribute("src", car.image);
+  carName.innerText = car.name;
+  carRate.innerText = car.rate;
+
+  carName.appendChild(carRate);
+  carImgText.appendChild(carName);
+  bookNowBtn.innerHTML = "Book Now &rarr;";
+
+  carCard.appendChild(carImg);
+  carCard.appendChild(carImgText);
+
+  console.log(carCard);
+  CarNodes.push(carCard);
+});
+
+CarNodes.forEach((element) => {
+  cards.appendChild(element);
+});
