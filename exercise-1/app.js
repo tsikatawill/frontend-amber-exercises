e5992fbaeecbe000e00c8a87d8c6baf4b77fa022
abbey eleazer <abbeyelazer@gmail.com>
2023-11-06 04:42:03 +0200

    added exercise-5 style sheets and js

---
+++ exercise-1/app.js
@@ -0,0 +1,16 @@
+//select buttons
+
+const arrowUpBtn = document.querySelector('.arrUp');
+const arrowDownBtn = document.querySelector('.arrDwn');
+
+
+arrowDownBtn.addEventListener('click', () => {
+  arrowDownBtn.style.display = 'none';
+  arrowUpBtn.style.display = 'block';
+
+});
+arrowUpBtn.addEventListener('click', () => {
+  arrowDownBtn.style.display = 'block';
+  arrowUpBtn.style.display = 'none';
+
+});
---
+++ exercise-5/script.js
@@ -0,0 +1,12 @@
+
+window.addEventListener('scroll', topScrollIndicator);
+
+const scrollElement = document.getElementById('scrollIndicator');
+
+const maxHeight = window.document.body.scrollHeight - window.innerHeight;
+
+function topScrollIndicator (e) {
+  const percentage = ((window.scrollY) / maxHeight) * 100;
+
+  scrollElement.style.width = percentage + '%';
+}
