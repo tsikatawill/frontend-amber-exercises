157ef11b58f1ad42dadb72ff3b878b454586ae95
abbey eleazer <abbeyelazer@gmail.com>
2023-12-07 11:30:18 +0200

    added some functionality and a few styles

--- exercise-3/app.js
+++ exercise-3/app.js
@@ -1,12 +1,19 @@
-let numOfItem = 0;
 
-let displayNumOfItem = document.getElementById('displayItem');
-let displayCartNum = document.getElementById('cartDis');
+const displayNumOfItem = document.getElementById('displayItem');
+const displayCartNum = document.getElementById('cartDis');
+
+let numOfItem = 0;
+let qty = 0;
+
+// Add button 
 
 document.getElementById('plus').addEventListener('click', ()=>{
   numOfItem += 1;
   displayNumOfItem.textContent = numOfItem;
 });
+
+
+// subtract button 
 
 document.getElementById('minus').addEventListener('click', ()=>{
   numOfItem += -1;
@@ -14,12 +21,16 @@ document.getElementById('minus').addEventListener('click', ()=>{
 });
 
 
+// button to add number of items into cart 
+
 document.getElementById('add').addEventListener('click', ()=>{
-  displayCartNum.textContent = displayNumOfItem.textContent;
+  // grab the number of item selected and add to number of item in the cart
+  qty += numOfItem;
+  displayCartNum.textContent = qty;
   
-  displayCartNum.style.display = 'block';
-   displayNumOfItem.textContent = 0;
-   numOfItem = 0;
+  displayCartNum.style.display = 'block';  //display the number of items in the cart on the ui
+   displayNumOfItem.textContent = 0;      //reset  to 0
+   numOfItem = 0;                         //reset to 0
 
 });
 
